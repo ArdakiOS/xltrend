32831bb85534b1be35c2958dae44263004b6aeea
Ardak <ardaktursunbayev4@gmail.com>
2024-04-13 20:17:46 +0600

    Initial Commit

---
+++ xltrend/xltrendApp.swift
@@ -0,0 +1,17 @@
+//
+//  xltrendApp.swift
+//  xltrend
+//
+//  Created by Ardak Tursunbayev on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct xltrendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
